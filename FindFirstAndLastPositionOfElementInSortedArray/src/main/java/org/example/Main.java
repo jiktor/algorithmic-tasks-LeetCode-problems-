b5d11957cf03b6d57cfa14b6a74987243940ebb6
jiktor <viktorjgeorgiev@gmail.com>
2023-12-12 13:35:32 +0200

    ReverseInteger problem

--- FindFirstAndLastPositionOfElementInSortedArray/src/main/java/org/example/Main.java
+++ FindFirstAndLastPositionOfElementInSortedArray/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
-		int [] arr = {1,2,2,2,3,4,4,5,6,6,6,6,7};
+		int [] arr = {1,2,2,2,3,4,4,4,4,4,4,4,5,6,6,6,6,7};
 		System.out.println(Arrays.toString(Solution.searchRange(arr,4)));
 	}
 }
