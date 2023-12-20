a47422b93b0f731c76c904b50ce4e400c3b896c0
jiktor <viktorjgeorgiev@gmail.com>
2023-12-20 12:18:09 +0200

    added solution for caontainer with max water problem

--- containerWithMostWater/src/main/java/org/example/Main.java
+++ containerWithMostWater/src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		int [] arr = new int[]{1,8,6,2,5,4,8,3,7};
+		System.out.println(Solution.maxArea(arr));
+	}
+
+	class Solution {
+		public static int maxArea(int[] height) {
+			int leftIndex = 0;
+			int rightIndex = height.length-1;
+			int result =0;
+			while(leftIndex != rightIndex){
+				if(height[rightIndex] > height[leftIndex]){
+					if(height[leftIndex]*(rightIndex-leftIndex)>result){
+						result = height[leftIndex]*(rightIndex-leftIndex);
+					}
+					leftIndex++;
+				}else{
+					if(height[rightIndex]*(rightIndex-leftIndex)>result){
+						result = height[rightIndex]*(rightIndex-leftIndex);
+					}
+					rightIndex--;
+				}
+			}
+			return result;
+		}
 	}
 }
