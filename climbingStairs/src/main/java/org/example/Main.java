8adadbbb4d35a889fb4826b744b68112ccedd5f1
jiktor <viktorjgeorgiev@gmail.com>
2024-02-12 19:02:29 +0200

    Dynamic problem solution for climbing stairs problem

---
+++ climbingStairs/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(Solution.climbStairs(4));
+	}
+	static class Solution {
+		public static int climbStairs(int n) {
+			int one = 1;
+			int two = 1;
+			for(int i=0;i<n-1;i++){
+				int temp = one;
+				one = one + two;
+				two = temp;
+			}
+
+			return one;
+		}
+	}
+}
